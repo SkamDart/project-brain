47d400f611f5d60bf0e8fca4d024a1b53abc0f3c
Skam <cdart2@illinois.edu>
2017-03-07 23:30:29 -0600

    initial commit for license plate attention network

---
+++ license_plate/README.md
@@ -0,0 +1,4 @@
+# License Plate Attention Network
+### Datasets
+[Hsu, G.-S.; Chen, J.-C.; Chung, Y.-Z.; , "Application-Oriented License Plate Recognition," Vehicular Technology, IEEE Transactions on , vol.62, no.2, pp.552-561, Feb. 2013](http://ieeexplore.ieee.org/xpl/articleDetails.jsp?reload=true&arnumber=6339122)
+[L. Dlagnekov and S. Belongie, "UCSD/Calit2 Car License Plate, Make and Model Database,"](http://vision.ucsd.edu/car_data.html)
---
+++ license_plate/license_plate.py
@@ -0,0 +1,15 @@
+"""Builds a license plate detection and classification model
+Architecture to attention network papers:
+"""
+
+# Basic model parameters.
+tf.app.flags.DEFINE_integer('batch_size', 128,
+                            """Number of images to process in a batch.""")
+tf.app.flags.DEFINE_string('data_dir', '/tmp/lp',
+                           """Path to the data directory containing license plate images.""")
+
+# Global constants describing the CIFAR-10 data set.
+IMAGE_SIZE = cifar10_input.IMAGE_SIZE
+NUM_CLASSES = cifar10_input.NUM_CLASSES
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = cifar10_input.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN
+NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = cifar10_input.NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
